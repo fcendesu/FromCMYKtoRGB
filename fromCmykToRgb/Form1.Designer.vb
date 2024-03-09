8b915eb92ea24321a2736d99aa8cb315e791286a
Furkan Çakmak <44734150+fcendesu@users.noreply.github.com>
2024-03-09 22:42:05 +0200

    Add files via upload

---
+++ fromCmykToRgb/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar2 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar3 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar4 = New System.Windows.Forms.HScrollBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(65, 63)
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(231, 21)
+        Me.HScrollBar1.TabIndex = 0
+        '
+        'HScrollBar2
+        '
+        Me.HScrollBar2.Location = New System.Drawing.Point(65, 140)
+        Me.HScrollBar2.Name = "HScrollBar2"
+        Me.HScrollBar2.Size = New System.Drawing.Size(231, 21)
+        Me.HScrollBar2.TabIndex = 1
+        '
+        'HScrollBar3
+        '
+        Me.HScrollBar3.Location = New System.Drawing.Point(65, 209)
+        Me.HScrollBar3.Name = "HScrollBar3"
+        Me.HScrollBar3.Size = New System.Drawing.Size(231, 21)
+        Me.HScrollBar3.TabIndex = 2
+        '
+        'HScrollBar4
+        '
+        Me.HScrollBar4.Location = New System.Drawing.Point(65, 279)
+        Me.HScrollBar4.Name = "HScrollBar4"
+        Me.HScrollBar4.Size = New System.Drawing.Size(231, 21)
+        Me.HScrollBar4.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(308, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(308, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(308, 213)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(308, 283)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Label4"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(402, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(358, 291)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.HScrollBar4)
+        Me.Controls.Add(Me.HScrollBar3)
+        Me.Controls.Add(Me.HScrollBar2)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents HScrollBar2 As HScrollBar
+    Friend WithEvents HScrollBar3 As HScrollBar
+    Friend WithEvents HScrollBar4 As HScrollBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ fromCmykToRgb/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Public Sub renk()
+        Dim cyan, magenta, yellow, black As Double
+        cyan = HScrollBar1.Value / 100
+        magenta = HScrollBar2.Value / 100
+        yellow = HScrollBar3.Value / 100
+        black = HScrollBar4.Value / 100
+
+        r = 255 * (1 - cyan) * (1 - black)
+        g = 255 * (1 - magenta) * (1 - black)
+        b = 255 * (1 - yellow) * (1 - black)
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "0%"
+        Label2.Text = "0%"
+        Label3.Text = "0%"
+        Label4.Text = "0%"
+
+        HScrollBar1.Maximum = 100
+        HScrollBar1.Minimum = 0
+        HScrollBar1.LargeChange = 1
+
+        HScrollBar2.Maximum = 100
+        HScrollBar2.Minimum = 0
+        HScrollBar2.LargeChange = 1
+
+        HScrollBar3.Maximum = 100
+        HScrollBar3.Minimum = 0
+        HScrollBar3.LargeChange = 1
+
+        HScrollBar4.Maximum = 100
+        HScrollBar4.Minimum = 0
+        HScrollBar4.LargeChange = 1
+
+    End Sub
+    Dim c, m, y, k As Double
+
+    Private Sub HScrollBar3_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar3.Scroll
+
+        'burda scroolbar içindeki değerleri yüze bölüyoruz ondalık değere çeviriyoruz örneğin scroolbar değeri
+        '32 100 bölüp 0.32 buluyoruz 
+        c = HScrollBar1.Value / 100
+        m = HScrollBar2.Value / 100
+        y = HScrollBar3.Value / 100
+        k = HScrollBar4.Value / 100
+
+        r = 255 * (1 - c) * (1 - k)
+        g = 255 * (1 - m) * (1 - k)
+        b = 255 * (1 - y) * (1 - k)
+
+        Label3.Text = HScrollBar3.Value.ToString + "%"
+
+        PictureBox1.BackColor = System.Drawing.Color.FromArgb(r, g, b)
+    End Sub
+
+    Private Sub HScrollBar4_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar4.Scroll
+        c = HScrollBar1.Value / 100
+        m = HScrollBar2.Value / 100
+        y = HScrollBar3.Value / 100
+        k = HScrollBar4.Value / 100
+
+        r = 255 * (1 - c) * (1 - k)
+        g = 255 * (1 - m) * (1 - k)
+        b = 255 * (1 - y) * (1 - k)
+
+        Label4.Text = HScrollBar4.Value.ToString + "%"
+
+        PictureBox1.BackColor = System.Drawing.Color.FromArgb(r, g, b)
+    End Sub
+
+    Private Sub HScrollBar2_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar2.Scroll
+        c = HScrollBar1.Value / 100
+        m = HScrollBar2.Value / 100
+        y = HScrollBar3.Value / 100
+        k = HScrollBar4.Value / 100
+
+        r = 255 * (1 - c) * (1 - k)
+        g = 255 * (1 - m) * (1 - k)
+        b = 255 * (1 - y) * (1 - k)
+
+        Label2.Text = HScrollBar2.Value.ToString + "%"
+
+        PictureBox1.BackColor = System.Drawing.Color.FromArgb(r, g, b)
+    End Sub
+
+    Dim r, g, b As Double
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        c = HScrollBar1.Value / 100
+        m = HScrollBar2.Value / 100
+        y = HScrollBar3.Value / 100
+        k = HScrollBar4.Value / 100
+
+        r = 255 * (1 - c) * (1 - k)
+        g = 255 * (1 - m) * (1 - k)
+        b = 255 * (1 - y) * (1 - k)
+
+        Label1.Text = HScrollBar1.Value.ToString + "%"
+
+        PictureBox1.BackColor = System.Drawing.Color.FromArgb(r, g, b)
+
+    End Sub
+End Class
